63a29cb2d877b20faa8972850b4e010c224e9283
Antônio Roberto Silva <forevertonny@gmail.com>
2012-07-10 20:15:11 -0300

    initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whatismyvideoid::Application.config.secret_token = '05e53b7a21b82f43dcbb72ffdb9ce0105fd51a7341219e5e772e8252ce8cec43d41564a598b40c587b3cc68daf5e47681e4586c8ceaac914dfee4fb0f953594d'
